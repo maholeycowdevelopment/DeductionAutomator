99d198ec3c6b988951a6062024c7d9cd58db34a8
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-07-08 12:27:55 -0600

    Changed the Account controller to redirect to Deduction when a user logs out

--- DeductionAutomator/Controllers/AccountController.cs
+++ DeductionAutomator/Controllers/AccountController.cs
@@ -247,7 +247,7 @@ namespace DeductionAutomator.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction(nameof(DeductionController.Index), "Deduction");
         }
 
         [HttpPost]
@@ -335,7 +335,7 @@ namespace DeductionAutomator.Controllers
         {
             if (userId == null || code == null)
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(DeductionController.Index), "Deduction");
             }
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
@@ -455,7 +455,7 @@ namespace DeductionAutomator.Controllers
             }
             else
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(DeductionController.Index), "Deduction");
             }
         }
 
